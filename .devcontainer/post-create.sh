774f139619e10e58acbe2f55156bd07b6903a79f
ThomasRanvikEriksen <thomasranvik.eriksen@lede.no>
2026-08-27 14:13:57 +0200

    feat: Update Chromium dependencies for Puppeteer SVG rendering

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,9 +3,10 @@ set -e
 
 # Chromium dependencies for puppeteer SVG rendering
 sudo apt-get update && sudo apt-get install -y --no-install-recommends \
-  libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libxdamage1 \
-  libxrandr2 libgbm1 libasound2 libpangocairo-1.0-0 libxshmfence1 \
-  libxkbcommon0 libxcomposite1 libpango-1.0-0 libcairo2 \
+  libnss3 libnspr4 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 \
+  libxdamage1 libxfixes3 libxrandr2 libgbm1 libasound2 libasound2t64 \
+  libpangocairo-1.0-0 libxshmfence1 libxkbcommon0 libxcomposite1 \
+  libpango-1.0-0 libcairo2 \
   && sudo rm -rf /var/lib/apt/lists/*
 
 pip install -r requirements.txt
