7e494a87660dc17c9cbf8776a0377099e5a84b24
ThomasRanvikEriksen <thomasranvik.eriksen@lede.no>
2026-08-27 13:27:44 +0000

    fix: Remove PowerShell startup script and install script; add generate_all_profiles.sh for profile generation

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# py -m venv venv # If you do not have venv installed. use this the first time
-source venv/Scripts/activate
-pip install -r requirements.txt
-pip install -e .
-
-# Run:
-# chmod +x install.sh
-# source install.sh
